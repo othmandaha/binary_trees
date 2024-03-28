7fe384c91d0d31df28e8051a5b5af97ea7fdf6e2
othmandaha <dahaothman02@gmail.com>
2024-03-28 16:12:07 +0000

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t rheight;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	lheight = binary_tree_height(tree->left);
 	rheight = binary_tree_height(tree->right);
